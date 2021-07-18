f7284f8eec1bd89ea68dab7a84403b7cfe84a8bc
nico <nico@guainazzo.it>
2021-07-18 15:20:41 +0200

    completate le transazioni sequenziali

--- File Java per progetto da giugno 2021-20210708.zip_expanded/labo.java
+++ File Java per progetto da giugno 2021-20210708.zip_expanded/labo.java
@@ -8,65 +8,199 @@ import java.util.*;
 class labo
 {
 
-
- public static void main (String args [])
- {
-     
-    // APERTURA CONNESSIONE -- CODICE DA MODIFICARE CON VOSTRI DATI 
-
-    String url = "jdbc:postgresql://130.251.61.30/YOUR_DBNAME"; 
-                                    //es. db bd2user_n
-	 String user ="YOUR_USERNAME"; //es. bd2userxx
-	 String pass ="YOUR_PASSWORD"; //es. BD2userxx
-
-     
-	 Connection conn = null;
-
-	// CARICAMENTO DRIVER
-     
-    try{
+public static Connection connect()throws SQLException {
+	String url = "jdbc:postgresql://127.0.0.1:5432/ProgettoDB2"; 
+	String user ="postgres";
+	String pass ="Nicogs43"; 
+	try{
 		Class.forName ("org.postgresql.Driver");
-
+	}catch(java.lang.ClassNotFoundException e) {
+		System.err.print("ClassNotFoundException: "); 
+		System.err.println(e.getMessage());
+	}
 	// CONNESSIONE
-
-	conn =  DriverManager.getConnection(url, user, pass);
-        
-    // EVENTUALE VARIAZIONE SCHEMA
-
-    //PreparedStatement st = conn.prepareStatement("set search_path to account");
-	//st.executeUpdate();
-
-    // INIZIALIZZAZIONE AUTOCOMMIT A FALSE PER IMPOSTARE COMPORTAMENTO TRANSAZIONALE
-	//conn.setAutoCommit(false);
-        
-        
-    // ESECUZIONE COMANDI
-    PreparedStatement st1 = conn.prepareStatement("INSERT INTO Account VALUES (0,0)");
-	st1.executeUpdate();
-    //PreparedStatement st2 = conn.prepareStatement("SELECT * FROM Account");
-    //st2.executeQuery();
-
-	// chiusura connessione
-	//	conn.commit();
-		conn.close();
-		
-}
-   	catch(java.lang.ClassNotFoundException e) {
-			System.err.print("ClassNotFoundException: "); 
-			System.err.println(e.getMessage());
-		}
+	return  DriverManager.getConnection(url, user, pass);
 	
-	catch (SQLException e1) {
+}
+
+public static void transactionOne() {
+	Connection conn=null;
+	try {
+		conn = connect();
+	} catch (SQLException e2) {
+		e2.printStackTrace();
+	}
+    // EVENTUALE VARIAZIONE SCHEMA
+     //PreparedStatement st = conn.prepareStatement("set search_path to account");
+     //st.executeUpdate();
+    // INIZIALIZZAZIONE AUTOCOMMIT A FALSE PER IMPOSTARE COMPORTAMENTO TRANSAZIONALE
+	 try {
+	conn.setAutoCommit(false);  
+    // ESECUZIONE COMANDO
+	
+    Statement st1 = conn.createStatement();
+    ResultSet rs = st1.executeQuery("SELECT \"Codice_fiscale\" , \"Nome\" FROM \"Cliente\" where \"Nome\" like 'Nico%'");
+    
+    while (rs.next()) {
+        String codFisc = rs.getString("Codice_fiscale");
+        String Name = rs.getString("Nome");
+        System.out.println(codFisc + Name + "     ");
+    }
+    
+    System.out.println("*******************************************************************");
+    
+    Statement st2 = conn.createStatement();
+    ResultSet rs1 = st2.executeQuery("SELECT \"Codice_fiscale\"  FROM \"Vendite\" Where \"Codice_fiscale\" like 'NI%' ");
+    
+    while (rs1.next()) {
+        String Name = rs1.getString("Codice_fiscale");
+        System.out.println(Name + "     ");
+    }
+    
+    System.out.println("*******************************************************************");
+    
+    Statement st3 = conn.createStatement();
+    ResultSet rs3 = st3.executeQuery("SELECT \"Codice_fiscale\" FROM \"Cliente\"  where \"Nome\" like 'N%'");
+    
+    while (rs3.next()) {
+        String Name = rs3.getString("Codice_fiscale");
+        System.out.println(Name + "     ");
+    }
+    
+	// chiusura connessione
+		conn.commit();
+		conn.close();
+	 }catch (SQLException e1) {
+		 e1.printStackTrace();
+	 
 		try{
-			if (conn != null) conn.rollback();
-			 } catch (SQLException e) {
+			if (conn != null) 
+				conn.rollback();
+			} catch (SQLException e) {
 			 		while( e!=null){ 
 						System.out.println("SQLState: " + e.getSQLState());
 						System.out.println("    Code: " + e.getErrorCode());
 						System.out.println(" Message: " + e.getMessage());
 						e = e.getNextException();
 						}	}
-        
+	 }
 				}
-   } 
+
+
+public static void transactionTwo() {
+	Connection conn=null;
+	try {
+		conn = connect();
+	} catch (SQLException e2) {
+		e2.printStackTrace();
+	}
+	 // INIZIALIZZAZIONE AUTOCOMMIT A FALSE PER IMPOSTARE COMPORTAMENTO TRANSAZIONALE
+	 try {
+	conn.setAutoCommit(false);
+	
+	
+	    Statement st1 = conn.createStatement();
+	    int i = st1.executeUpdate("INSERT INTO \"Cliente\"(\"Nome\" , \"Cognome\" , \"Codice_fiscale\" , \"Sconto\" , \"Note\") VALUES ('Nicolo','Guai','GNZNCL27','30','Ok')");
+	    if(i==1) {
+	    	 System.out.println("Insert riuscita");
+	    }
+
+	
+	    Statement st2 = conn.createStatement();
+	    ResultSet rs1 = st2.executeQuery("Select \"Nome\",\"Codice_fiscale\" From \"Cliente\" where  \"Nome\" like 'N%' ");
+	    
+	    while (rs1.next()) {
+	        String Name = rs1.getString("Nome");
+	        String CodFisc = rs1.getString("Codice_fiscale");
+
+	        System.out.println(Name + " " + CodFisc+ "     ");
+	    }
+	    
+	    Statement st3 = conn.createStatement();
+	    int test = st3.executeUpdate("Delete From \"Cliente\" Where \"Codice_fiscale\" = 'GNZNCL27' ");
+	    if(test==1) {
+	    	 System.out.println("Delete riuscita");
+	    }
+	    
+
+	conn.commit();
+	conn.close();
+ }catch (SQLException e1) {
+	 e1.printStackTrace();
+	try{
+		if (conn != null) conn.rollback();
+		 } catch (SQLException e) {
+		 		while( e!=null){ 
+					System.out.println("SQLState: " + e.getSQLState());
+					System.out.println("    Code: " + e.getErrorCode());
+					System.out.println(" Message: " + e.getMessage());
+					e = e.getNextException();
+					}	}
+    
+			}
 }
+
+public static void transactionThree() {
+	Connection conn=null;
+	try {
+		conn = connect();
+	} catch (SQLException e2) {
+		e2.printStackTrace();
+	}
+	 // INIZIALIZZAZIONE AUTOCOMMIT A FALSE PER IMPOSTARE COMPORTAMENTO TRANSAZIONALE
+	 try {
+	conn.setAutoCommit(false);
+	
+	  Statement st1 = conn.createStatement();
+	    int i = st1.executeUpdate("INSERT INTO \"Vendite\" ( \"Codice_fiscale\" , \"Data\" , \"Quantità\", \"Tipo\" , \"Marca\") VALUES ('GNZNCL27','2021-07-18', 40 ,'Pasta', 'rummo')");
+	    if(i==1) {
+	    	 System.out.println("Insert riuscita");
+	    }
+	    
+	    Statement st2 = conn.createStatement();
+	    int ii = st2.executeUpdate("UPDATE \"Cliente\" Set \"Sconto\" = 30 Where \"Nome\" Like 'Nico%'");
+	    if(ii==1) {
+	    	 System.out.println("Update riuscita");
+	    }
+	    
+	    Statement st3 = conn.createStatement();
+	    int test = st3.executeUpdate("Delete From \"Vendite\" Where \"Codice_fiscale\" = 'GNZNCL27' ");
+	    if(test==1) {
+	    	 System.out.println("Delete riuscita");
+	    }
+
+	    Statement st4 = conn.createStatement();
+	    int iii = st4.executeUpdate("UPDATE \"Cliente\" Set \"Sconto\" = 10 Where \"Nome\" Like 'Nico%'");
+	    if(iii==1) {
+	    	 System.out.println("Update riuscita");
+	    }
+	conn.commit();
+	conn.close();
+ }catch (SQLException e1) {
+	 e1.printStackTrace();
+	try{
+		if (conn != null) conn.rollback();
+		 } catch (SQLException e) {
+		 		while( e!=null){ 
+					System.out.println("SQLState: " + e.getSQLState());
+					System.out.println("    Code: " + e.getErrorCode());
+					System.out.println(" Message: " + e.getMessage());
+					e = e.getNextException();
+					}	}
+    
+			}
+}
+
+	
+	
+
+ public static void main (String args [])
+ {
+     
+    // APERTURA CONNESSIONE -- CODICE DA MODIFICARE CON VOSTRI DATI 
+	 transactionOne();
+	 transactionTwo();
+	 transactionThree();
+   }
+}
+
